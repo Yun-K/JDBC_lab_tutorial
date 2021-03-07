5de972210bacc8ad68d50d49f1cd0112c909bca6
Yun Zhou <YunZhou0612@qq.com>
2021-03-07 23:04:08 +0800

    Add FetchStudents class and methods with throw exception

---
+++ jdbc_lab1_swen301/src/jdbc_lab1_swen301/FetchStudents.java
@@ -0,0 +1,20 @@
+package jdbc_lab1_swen301;
+
+public class FetchStudents {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+    public static void fetchAll() throws Exception {
+
+    }
+
+    public static void fetchById(int id) throws Exception {
+
+    }
+
+    public static void fetchByDegreeAndMajor(String degree, String major) throws Exception {
+
+    }
+}
